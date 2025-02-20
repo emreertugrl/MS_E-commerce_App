574a4c7975ed235ed83a981ac59b2d571922c4ea
emreertugrl <emrertugrl@gmail.com>
2025-02-20 23:27:56 +0300

    rabbitMq connection completed

--- auth/src/auth.service.js
+++ auth/src/auth.service.js
@@ -1,9 +1,22 @@
+const amqp = require("amqplib");
 // business logic'i ve veritabanı ile iletişime geçecek olan katman.
 class AuthService {
+  constructor() {
+    this.initializeRabbitMq(); // RabbitMq bağlantısı başlatılır(hemen çalıştırmak için)
+  }
+  // RabbitMq bağlantı fonksiyonu
+  async initializeRabbitMq() {
+    try {
+      const connection = await amqp.connect(process.env.RabbitMQ_URL);
+      console.log("RabbitMQ bağlandı");
+    } catch (error) {
+      console.error("RabbitMQ Bağlantı hatası:", error);
+    }
+  }
   static async register() {}
   static async login() {}
   static async refresh() {}
   static async logout() {}
 }
 
-module.exports = AuthService;
+module.exports = new AuthService();
