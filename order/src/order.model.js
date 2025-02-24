dfc75913dc37d6c378811a4f576e57a5c7479468
emreertugrl <emrertugrl@gmail.com>
2025-02-24 15:21:53 +0300

    order model defined

--- order/src/order.model.js
+++ order/src/order.model.js
@@ -1,30 +1,58 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt.js");
 
 const orderSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-    password: { type: String, required: true, minLength: 6 },
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
-    role: { type: String, required: true, enum: ["order", "admin"], default: "order" },
-    refreshToken: { type: String },
-    isActive: { type: Boolean, default: true },
-    lastLogin: { type: Date },
+    user: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: String,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["pending", "processing", "completed", "cancelled"],
+      default: "pending",
+    },
+    destination: {
+      city: { type: String, required: true },
+      district: { type: String, required: true },
+      street: { type: String, required: true },
+      apartment: { type: Number, required: true },
+      flat: { type: Number, required: true },
+    },
   },
   {
     timestamps: true,
   }
 );
 
-// client'a cevap göndermeden önce hassas verileri gizle
+// client'a cevap göndermden önce hassas verileri gizle
 orderSchema.methods.toJSON = function () {
   const obj = this.toObject();
   delete obj.__v;
   return obj;
 };
-// module oluşturulur.
 
+// model oluştur
 const Order = mongoose.model("Order", orderSchema);
 
 module.exports = Order;
