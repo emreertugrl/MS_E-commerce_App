74cb98b5f6b6cf1843fcb716c3563f19a0700c9b
emreertugrl <emrertugrl@gmail.com>
2025-02-24 19:58:19 +0300

    order joi defined

--- order/src/order.controller.js
+++ order/src/order.controller.js
@@ -1,10 +1,12 @@
 const OrderService = require("./order.service");
 
 class OrderController {
-  async createOrder(req, res) {}
-  async getOrder(req, res) {}
-  async getUserOrders(req, res) {}
-  async updateOrderStatus(req, res) {}
+  async createOrder(req, res, next) {
+    const orderData = req.body;
+  }
+  async getOrder(req, res, next) {}
+  async getUserOrders(req, res, next) {}
+  async updateOrderStatus(req, res, next) {}
 }
 
 // örneğini alıp export ederek diğer tarafta kullanabiliriz.
